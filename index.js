2dc3b249f809ee88507695dfd5f59501096a59b5
Tendai Mutunhire <tmutunhire@gmail.com>
2017-03-07 13:51:39 +0200

    components

--- index.js
+++ index.js
@@ -41,3 +41,26 @@ var app5 = new Vue({
     }
   }
 });
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Rockin a Masserati!'
+  }
+});
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+});
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { text: 'Vegetables' },
+      { text: 'Cheese' },
+      { text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+});
